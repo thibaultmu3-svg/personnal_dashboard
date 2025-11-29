b9a593b731e9b7ffc4502c11b0ecd23a84ecdcb9
thibaultmu3-svg <thibaultmu3@gmail.com>
2025-11-29 12:51:57 +0100

    rendre event pour mobile

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ navbarElements.forEach((el) => {
     })
 })
 
+
+
+navbarElements.forEach((el) => {
+    el.addEventListener("touchend", () => {
+
+        //partie navbar
+        el.classList.add("active")
+        navbar_active.classList.remove("active")
+
+        //partie content
+        const new_content = document.querySelector(`.${el.id}`)
+        new_content.classList.add("active")
+
+        const old_content = document.querySelector(`.${navbar_active.id}`)
+        console.log(old_content)
+        old_content.classList.remove("active")
+
+        navbar_active = el
+    })
+})
