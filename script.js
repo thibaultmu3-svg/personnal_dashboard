cd472f352445523e52629831f2b62a24ea72fa6a
thibaultmu3-svg <thibaultmu3@gmail.com>
2025-11-29 13:18:26 +0100

    Ce sera juste pour le pc

--- script.js
+++ script.js
@@ -19,24 +19,3 @@ navbarElements.forEach((el) => {
         navbar_active = el
     })
 })
-
-
-
-navbarElements.forEach((el) => {
-    el.addEventListener("touchstart", () => {
-
-        //partie navbar
-        el.classList.add("active")
-        navbar_active.classList.remove("active")
-
-        //partie content
-        const new_content = document.querySelector(`.${el.id}`)
-        new_content.classList.add("active")
-
-        const old_content = document.querySelector(`.${navbar_active.id}`)
-        console.log(old_content)
-        old_content.classList.remove("active")
-
-        navbar_active = el
-    })
-})
