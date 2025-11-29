425a286f24c119e33961fde5840558aa4853673e
thibaultmu3-svg <thibaultmu3@gmail.com>
2025-11-29 12:19:43 +0100

    adapter html id

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(visualViewport.width, visualViewport.height)
+const navbarElements = document.querySelectorAll(".class")
+let navbar_active = document.querySelector(".class.active")
+
+navbarElements.forEach((el) => {
+    el.addEventListener("click", () => {
+
+        //partie navbar
+        el.classList.add("active")
+        navbar_active.classList.remove("active")
+
+        //partie content
+        const new_content = document.querySelector(".content.active")
+        console.log(new_content)
+
+        const old_content = document.getElementById(navbar_active.id)
+        old_content.classList.remove("active")
+
+        navbar_active = el
+    })
+})
+// pas tres correct je pense
