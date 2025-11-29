6cd1286c609d315406e4887548d74dff5f07f902
thibaultmu3-svg <thibaultmu3@gmail.com>
2025-11-29 13:16:12 +0100

    test

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 const navbarElements = document.querySelectorAll(".class")
 let navbar_active = document.querySelector(".class.active")
 
-navbarElements.forEach((el) => {
-    el.addEventListener("click", () => {
-
-        //partie navbar
-        el.classList.add("active")
-        navbar_active.classList.remove("active")
-
-        //partie content
-        const new_content = document.querySelector(`.${el.id}`)
-        new_content.classList.add("active")
-
-        const old_content = document.querySelector(`.${navbar_active.id}`)
-        console.log(old_content)
-        old_content.classList.remove("active")
-
-        navbar_active = el
-    })
-})
-
-
 
 navbarElements.forEach((el) => {
     el.addEventListener("pointerdown", () => {
