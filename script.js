ded3bb37ada53ea0a05cd7b7aac5830d07ef7f6f
thibaultmu3-svg <thibaultmu3@gmail.com>
2025-11-29 13:17:54 +0100

    Es ce que ca va etre bon pour la tablette et pc ?????

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const navbarElements = document.querySelectorAll(".class")
 let navbar_active = document.querySelector(".class.active")
 
+navbarElements.forEach((el) => {
+    el.addEventListener("click", () => {
+
+        //partie navbar
+        el.classList.add("active")
+        navbar_active.classList.remove("active")
+
+        //partie content
+        const new_content = document.querySelector(`.${el.id}`)
+        new_content.classList.add("active")
+
+        const old_content = document.querySelector(`.${navbar_active.id}`)
+        console.log(old_content)
+        old_content.classList.remove("active")
+
+        navbar_active = el
+    })
+})
+
+
 
 navbarElements.forEach((el) => {
-    el.addEventListener("pointerdown", () => {
+    el.addEventListener("touchstart", () => {
 
         //partie navbar
         el.classList.add("active")
