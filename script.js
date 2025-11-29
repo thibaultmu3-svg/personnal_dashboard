7fef48427f3da3f07c525232b1f0262351a2d6de
thibaultmu3-svg <thibaultmu3@gmail.com>
2025-11-29 13:02:48 +0100

    test4 pour iphone

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 const navbarElements = document.querySelectorAll(".class")
 let navbar_active = document.querySelector(".class.active")
 
-navbarElements.forEach((el) => {
-    el.addEventListener("click", () => {
+// navbarElements.forEach((el) => {
+//     el.addEventListener("click", () => {
 
-        //partie navbar
+//         //partie navbar
+//         el.classList.add("active")
+//         navbar_active.classList.remove("active")
+
+//         //partie content
+//         const new_content = document.querySelector(`.${el.id}`)
+//         new_content.classList.add("active")
+
+//         const old_content = document.querySelector(`.${navbar_active.id}`)
+//         console.log(old_content)
+//         old_content.classList.remove("active")
+
+//         navbar_active = el
+//     })
+// })
+
+
+
+// navbarElements.forEach((el) => {
+//     el.addEventListener("pointerdown", () => {
+
+//         //partie navbar
+//         el.classList.add("active")
+//         navbar_active.classList.remove("active")
+
+//         //partie content
+//         const new_content = document.querySelector(`.${el.id}`)
+//         new_content.classList.add("active")
+
+//         const old_content = document.querySelector(`.${navbar_active.id}`)
+//         console.log(old_content)
+//         old_content.classList.remove("active")
+
+//         navbar_active = el
+//     })
+// })
+
+
+
+navbarElements.forEach((el) => {
+    const activate = () => {
         el.classList.add("active")
         navbar_active.classList.remove("active")
 
@@ -17,26 +57,9 @@ navbarElements.forEach((el) => {
         old_content.classList.remove("active")
 
         navbar_active = el
-    })
-})
+    }
 
-
-
-navbarElements.forEach((el) => {
-    el.addEventListener("pointerdown", () => {
-
-        //partie navbar
-        el.classList.add("active")
-        navbar_active.classList.remove("active")
-
-        //partie content
-        const new_content = document.querySelector(`.${el.id}`)
-        new_content.classList.add("active")
-
-        const old_content = document.querySelector(`.${navbar_active.id}`)
-        console.log(old_content)
-        old_content.classList.remove("active")
-
-        navbar_active = el
-    })
+    el.addEventListener("touchstart", activate, { passive: true })  
+    el.addEventListener("pointerdown", activate)
+    el.addEventListener("click", activate)
 })
